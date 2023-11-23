9de9e9a7bb192a6734eaa739f83927519d1f55af
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2023-11-23 13:15:00 +0100

    add score board

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,14 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+function printScore(score){
+	let divPc = document.createElement('div');
+	divPc.innerHTML = score[0];
+	document.getElementById('computer-result').innerHTML = '';
+	document.getElementById('computer-result').appendChild(divPc);
+
+	let divPlayer = document.createElement('div');
+	divPlayer.innerHTML = score[1];
+	document.getElementById('player-result').innerHTML = '';
+	document.getElementById('player-result').appendChild(divPlayer);
+}
--- js/script.js
+++ js/script.js
@@ -25,20 +25,26 @@ function playGame(playerInput){
         //sciezka kamienia
         if (argComputerMove == 'kamień' && argPlayerMove == 'papier'){
             printMessage('Ty wygrywasz');
+            score[1]++;
         } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
             printMessage('Komputer wygrywa');
+            score[0]++;
         }
         //sciezka papieru
         if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
             printMessage('Komputer wygrywa');
+            score[0]++;
         } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
             printMessage('Ty wygrywasz');
+            score[1]++;
         }
         //sciezka nozyc
         if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
             printMessage('Komputer wygrywa');
+            score[0]++;
         } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
             printMessage('Ty wygrywasz');
+            score[1]++;
         }
 
     }
@@ -48,8 +54,16 @@ function playGame(playerInput){
     let playerMove = getMoveName(playerInput);
 
     displayResult(computerMove, playerMove)
+    printScore(score);
+
 }
+
+let score = [0,0];
+printScore(score);
 
 document.getElementById('play-rock').addEventListener('click', function(){playGame(1)})
 document.getElementById('play-paper').addEventListener('click', function(){playGame(2)})
 document.getElementById('play-scissors').addEventListener('click', function(){playGame(3)})
+
+
+
